dac3e3235ecb135704830728983dbd431f8803a0
icreatesparkteam <kavita.khanna@lnttechservices.com>
2015-07-21 20:50:56 -0400

    Added api mapping for login page

---
+++ WebContent/js/config.js
@@ -0,0 +1,10 @@
+var config = {
+	"Server_IP" : "http://localhost:8080",
+	"loginURL" : "/iControlE-Core/rest/auth/login",
+	"logoutURL" : "/iControlE-Core/rest/auth/logout",
+	"createServiceProvider" : "/iControlE-Core/rest/registration/create",
+	"updateServiceProvider" : "/iControlE-Core/rest/registration/update",
+	"passwordRecovery" : "/iControlE-Core/rest/registration/passwordRecovery",
+	"changepassword" : "/iControlE-Core/rest/registration/changepassword",
+	"serviceproviderlist" : "/iControlE-Core/rest/registration/serviceproviderlist"
+}
